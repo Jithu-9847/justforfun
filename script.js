a2a6597c0a290e922ae8cf3b99a4d2ef872706ce
Jithu-9847 <131839320+Jithu-9847@users.noreply.github.com>
2025-03-05 14:27:11 +0530

    Change in jS

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
-function tap() {
-    x=Math.floor(Math.random() * width);
-    y=Math.floor(Math.random() * height);
-    
-    
-    document.getElementById("no").style.left=x+"px";
-    document.getElementById("no").style.top=y+"px";
-}
-let width=0;
-let height=0;
-let x=0;
-let y=0;
-let taped=false;
+let width;
+let height;
+let x;
+let y;
+
 window.onload=()=>{
-
-    width=window.screen.width;
-    height=window.screen.availHeight-100;
-    
-
+    width=window.screen.availWidth-100
+    height=window.screen.availHeight-100
 }
 
+function change() {
+    x=Math.floor(Math.random()*width)
+    y=Math.floor(Math.random()*height)
+
+    document.getElementById("no").style.left=x+"px"
+    document.getElementById("no").style.top=y+"px"
+}
 document.getElementById("no").addEventListener("mouseover",()=>{
-    if(taped==false)
-    taped=true;
-else
-    tap();
+    change()
 })
 
-document.getElementById("no").addEventListener("click",()=>{
-     
-    taped=true;
-    tap();
-})
 document.getElementById("yes").addEventListener("click",()=>{
-     
-    document.getElementById("yes").style.display="none";
-    document.getElementById("no").style.display="none";
-    document.getElementById("ms").style.display="block";
+    document.getElementById("head").style.display="none"
+    document.getElementById("yes").style.display="none"
+    document.getElementById("no").style.display="none"
+    document.getElementById("ms").style.display="block"
 })
